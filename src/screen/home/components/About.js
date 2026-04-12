1e72cbf5256b686edadab70d78c90a101137bda9
Manuel Cappai <manuelcappai@outlook.it>
2026-04-12 14:37:01 +0200

    changed the cv link

--- src/screen/home/components/About.js
+++ src/screen/home/components/About.js
@@ -22,7 +22,7 @@ function About({aboutRef}) {
                 I also love to explore new sides of development <span className='commented'>{'[--in the future--]'}</span>, I&#8217;d like to dive deeper into <span className='highlighted'>backend technologies</span> and even <span className='highlighted'>low-level programming</span>, just to understand how things really work behind the scenes. <br/><br/>
               <span className='string' style={{fontWeight: "bold"}}>"I want to become a better Developer!"</span>
               <br/><br/> Check my <a 
-                href="https://1drv.ms/b/c/5e037bc7a8950ec9/IQBVJ2TTruPVTIeZa5LW-faPAZINY60bg6PSfkmKpcOD1To?e=Xb7Aau"
+                href="https://1drv.ms/b/c/5e037bc7a8950ec9/IQDf5isFCkxZTrKBowjd_AyRAaCTLSkarO-RMg73MaaE9_I?e=bfGHxe"
                 target='_blank'
                 rel='noopener noreferrer'
                 className='commented'>
